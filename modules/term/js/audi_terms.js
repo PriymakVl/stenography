85012f21958255822d018f09c0c62e1b246fcb34
unknown <C:\Users\\AppData\Roaming\The Bat!\Mail>
2019-07-29 16:46:57 +0300

    29-07-19-work

---
+++ modules/term/js/audi_terms.js
@@ -0,0 +1,27 @@
+var sndLetItSnow = new Audio("sounds/1mp.mp3");
+	var sndSanta = new Audio("sounds/3mp.mp3");
+
+  var playlist = [sndLetItSnow, sndSanta];
+
+  var current = null;
+  var idx = 0;
+
+  function playSound() {
+      if (current === null || current.ended) {
+          // go to next
+          current = playlist[idx++];
+
+          // check if is the last of playlist and return to first
+          if (idx >= playlist.length)
+              idx = 0;
+
+           // return to begin
+           current.currentTime=0;
+
+           // play
+           current.play();
+      }
+
+  }
+
+  setInterval(playSound, 5000);
